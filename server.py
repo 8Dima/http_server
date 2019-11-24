b5f576ac9824439ab4799589a8b37b4e7f209b2c
Dmitriy <smphag@gmail.com>
2019-11-25 01:15:28 +0300

    404 page + http format date generator

--- server.py
+++ server.py
@@ -1,4 +1,25 @@
 import socket
+from datetime import datetime
+
+def http_format_date():
+	date_str = "Date: "
+	date = datetime.now()
+	days = {0: "Mon", 1: "Tue", 2: "Wed", 3: "Thu", 4: "Fri", 5: "Sat", 6: "Sun"}
+	months = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
+	day = days[date.weekday()]
+	month = months[date.month]
+	date_str+= day+", "+str(date.day)+" "+month+" "+str(date.year)
+
+	return date_str
+
+
+def response(filename, resp):
+	resp+=http_format_date()+"\n\n"
+	with open(filename, 'r') as file:
+			for line in file.readlines():
+				resp+=line
+			file.close()
+	return(resp)
 
 sock = socket.socket()
 
@@ -6,7 +27,7 @@ try:
 	sock.bind(('',80))
 	print("Using port 80")
 except OSError:
-	sock.bind(('', 8080))
+	sock.bind(('', 8081))
 	print("Using port 8080")
 
 sock.listen(5)
@@ -14,46 +35,36 @@ while True:
 	conn, addr = sock.accept()
 	print("Connected", addr)
 
-
-
 	data = conn.recv(8192)
 	msg = data.decode()
 	print(msg)
 
-	resp = """HTTP/1.1 200 OK
-	Server: SelfMadeServer v0.0.1
-	Content-type: text/html
+	print(http_format_date())
 
-	"""
+	filename = msg.split()[1:2][0][1:]
+	if filename == "":
+		filename = 'index.html'
 
-	if ("GET /index.html" in msg):
-		with open('index.html', 'r') as file:
-			for line in file.readlines():
-				resp+=line
+		resp = """HTTP/1.1 200 OK
+Server: SelfMadeServer v0.0.1
+Content-type: text/html
+"""
 
-	if ("GET /1.html" in msg):
-		with open('1.html', 'r') as file:
-			for line in file.readlines():
-				resp+=line
-	
-	if ("GET /2.html" in msg):
-		with open('2.html', 'r') as file:
-			for line in file.readlines():
-				resp+=line
+	if ".css" in filename:	
+		resp = """HTTP/1.1 200 OK
+Server: SelfMadeServer v0.0.1
+Content-type: text/css
+"""
 
-	if ("GET /style.css" in msg):
-		with open('style.css', 'r') as file:
-			for line in file.readlines():
-				resp+=line									
-
-	if ("/favicon.ico" in msg):
+	try:
+		resp = response(filename, resp)
+	except:
 		resp = """HTTP/1.1 404 Not Found
-		Server: SelfMadeServer v0.0.1
-		Content-type: text/html
+Server: SelfMadeServer v0.0.1
+Content-type: text/html
+"""
+		resp = response('404.html', resp)								
 
-		"""
-
-
-
+	print(resp)
 	conn.send(resp.encode())
 	conn.close()
